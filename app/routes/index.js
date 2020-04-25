5a319bde82ae5b423bb7c8a3e08692ffaacbe333
Arildo <0040117@academico.ifmg.edu.br>
2020-04-25 17:18:44 -0300

    node

--- app/routes/index.js
+++ app/routes/index.js
@@ -1,6 +1,6 @@
 module.exports = function (application) {
 	application.get('/', function (req, res) {
-		application.app.controllers.index.index(application, req, res);
+		application.controllers.index.index(application,req,res)
 	});
 
 
