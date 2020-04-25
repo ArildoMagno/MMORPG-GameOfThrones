865b9760222c94fcdc4a0823c35761c6eb9d83e9
Arildo <0040117@academico.ifmg.edu.br>
2020-04-25 17:37:17 -0300

    finish

--- app/routes/index.js
+++ app/routes/index.js
@@ -1,11 +1,11 @@
 module.exports = function (application) {
 	application.get('/', function (req, res) {
-		application.controllers.index.index(application,req,res)
+		application.app.controllers.index.index(application,req,res)
 	});
 
 
 
 	application.post('/autenticar', function (req, res) {
-		application.controllers.index.autenticar(application, req, res);
+		application.app.controllers.index.autenticar(application, req, res);
 	});
 }
